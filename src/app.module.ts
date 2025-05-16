cfae2a369f67450bcff82098088f5c2389d32ce4
seonghoo1217 <lsh6451217@gmail.com>
2025-05-17 01:50:59 +0900

    #1 style: 불필요한 주석 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,8 @@ import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
-    // .env 로부터 MONGO_URI 를 읽어올 수 있게 해 주는 설정
     ConfigModule.forRoot({ isGlobal: true }),
 
-    // MongoDB 커넥션 설정 — 반드시 한 번만 전역으로 등록
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -18,7 +16,6 @@ import { AuthModule } from './modules/auth/auth.module';
       }),
     }),
 
-    // AuthModule 등 기능별 모듈들
     AuthModule,
   ],
 })
