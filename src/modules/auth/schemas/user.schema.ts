e5b3996b2197369daecf3978cc41cf5365cb00bd
seonghoo1217 <lsh6451217@gmail.com>
2025-05-17 15:25:30 +0900

    #1 refactor: User Schema Document 상속 및 Mongoose 생성 변경
    - HydratedDocument를 사용하여 스키마 구성
    - Mongoose 설정 변경 및 클래스 load 사용

--- src/modules/auth/schemas/user.schema.ts
+++ src/modules/auth/schemas/user.schema.ts
@@ -1,19 +1,19 @@
 //prettier-ignore
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { Role } from './role.enum';
 
-export interface UserDocument extends User, Document {
-  toObject(): Record<string, any>;
-}
-
-@Schema({ timestamps: true })
+@Schema({
+  timestamps: true,
+  versionKey: false,
+  toJSON: { virtuals: true },
+})
 export class User {
   @Prop({ unique: true, required: true })
   username: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, select: false })
   password: string;
 
   @Prop({
@@ -42,10 +42,11 @@ export class User {
     return this.refreshToken === token && this.refreshTokenExpiry > new Date();
   }
 }
-
+export type UserDocument = HydratedDocument<User>;
 export const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.loadClass(User);
 
-// 비밀번호 해시 메소드를 문서 저장 전 hook으로 자동 실행
+// 비밀번호 해시 메소드를 문서 저장 전 hook으로 자동 실행하는 코드 save 기반
 UserSchema.pre<UserDocument>('save', async function (next) {
   if (this.isModified('password')) {
     const salt = await bcrypt.genSalt();
